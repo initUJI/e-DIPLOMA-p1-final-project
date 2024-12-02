f023da9806ccd3a330c5452e56cdf76e47059b00
almtav08 <alexmartinez3107@gmail.com>
2024-12-02 12:16:28 +0100

    remove unnecesary files

--- socket-server-master/server.js
+++ socket-server-master/server.js
@@ -14,6 +14,7 @@ const io = new Server(app, {
 const userRooms = {};
 const userPlayerNumber = {};
 const userPlayed = {};
+const userCar = {};
 let currentRoom = 1;
 
 // Dos usuarios se conectan a la misma sala.
@@ -24,51 +25,84 @@ let currentRoom = 1;
 // Cuando los dos usuarios se han unido a la sala, se emite un mensaje de room-ready.
 // Cuando un usuario se desconecta, se elimina la sala.
 io.on("connection", (socket) => {
+  // Obtenemos el usuario que se conecta y cuantos usuarios hay en la primera sala disponible.
   const user = socket.handshake.query.user;
   let room = `room-${currentRoom}`;
   const roomUsers = io.sockets.adapter.rooms.get(room)?.size || 0;
-  if (!userRooms[user]) {
-    if (roomUsers >= process.env.MAX_USERS_ROOM) {
-      currentRoom++;
-    }
-    room = `room-${currentRoom}`;
-    userRooms[user] = room;
-    userPlayed[user] = false;
-    userPlayerNumber[user] = roomUsers + 1;
-    socket.join(room);
-    socket.emit("joined-room", {
-      room: userRooms[user],
-      user: userPlayerNumber[user],
-    });
-  } else {
-    socket.emit("joined-room", {
-      room: userRooms[user],
-      user: userPlayerNumber[user],
-    });
-  }
 
+  // Si el tamaño de la sala es mayor o igual al máximo de usuarios por sala, se crea una nueva sala.
+  // if (roomUsers >= process.env.MAX_USERS_ROOM) {
+  //   currentRoom++;
+  //   room = `room-${currentRoom}`;
+  //   roomUsers = 0;
+  // }
+
+  // El jugador se conecta a la sala correspondiente. Se le asigna un número de jugador y se une a la sala.
+  userRooms[user] = room;
+  userPlayed[user] = false;
+  userPlayerNumber[user] = roomUsers + 1;
+  socket.join(room);
+  socket.emit("joined-room", {
+    room: userRooms[user],
+    user: userPlayerNumber[user],
+  });
   console.log(
     `User ${user} connected to room ${room}. Total users connected ${
       io.sockets.adapter.rooms.get(userRooms[user])?.size
     } / ${process.env.MAX_USERS_ROOM}`
   );
 
+  // Cuando la sala está llena, se emite un mensaje de room-ready. Se actualiza el numero de sala.
   if (
     io.sockets.adapter.rooms.get(userRooms[user])?.size >=
     process.env.MAX_USERS_ROOM
   ) {
     console.log(`Room ${userRooms[user]} is ready`);
     io.to(userRooms[user]).emit("room-ready");
+    currentRoom++;
   }
 
+  // El primer jugador que se conecta a la sala debe decidir el nivel que se va a jugar.
+  socket.on("select-level", (level) => {
+    console.log(`User ${user} selected level ${level}`);
+    io.to(userRooms[user]).emit("level", level);
+  });
+
+  // Esperar a que los dos jugadores completen el coche.
+  socket.on("car-ready", () => {
+    userCar[user] = true;
+    console.log(`${user} car is ready. Waiting for the other player.`);
+    // Get all users of the same room as the usercle
+    const currentUserRoom = userRooms[user];
+    const usersInRoom = Object.keys(userRooms).filter(
+      (u) => userRooms[u] === currentUserRoom
+    );
+    usersInRoom.forEach((u) => {
+      console.log(`User ${u} ready: ${userCar[u]} in room ${currentUserRoom}`); // Log user readiness
+    });
+
+    // Check if all users in the room are ready
+    const allUsersReady = usersInRoom.every((u) => userCar[u]);
+    if (allUsersReady && usersInRoom.length == process.env.MAX_USERS_ROOM) {
+      io.to(userRooms[user]).emit("start");
+      console.log("Both players' car are ready. Starting...");
+      usersInRoom.forEach((u) => {
+        userCar[u] = false;
+      });
+    }
+  });
+
+  // Cuando un jugador añade bloques, se envía un mensaje al otro jugador de la sala.
   socket.on("update", (msg) => {
     socket.to(userRooms[user]).emit("move", msg);
   });
 
+  // Cuando un jugador está listo, se actualiza el estado de la sala y se comprueba si ambos jugadores están listos.
+  // Si ambos están listos, se envía un mensaje para ejecutar el código.
   socket.on("ready", () => {
     userPlayed[user] = true;
     console.log(`${user} is ready. Waiting for the other player.`);
-    // Get all users of the same room as the user
+    // Get all users of the same room as the usercle
     const currentUserRoom = userRooms[user];
     const usersInRoom = Object.keys(userRooms).filter(
       (u) => userRooms[u] === currentUserRoom
@@ -90,6 +124,7 @@ io.on("connection", (socket) => {
     }
   });
 
+  // Cuando los jugadores de la sala se desconectan se elimina la sala.
   socket.on("disconnect", () => {
     console.log(`User ${user} disconnected from room ${room}`);
     if (!io.sockets.adapter.rooms.get(room)) {
